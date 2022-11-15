3063358ae3a3a4a3eb03f65c23952f648275edb0
kingspin-sudo <workwithwaqar@gmail.com>
2022-11-15 20:55:48 +0500

    Code Added

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+//    Print 1.....100 without using any numbers in your code
+    public static void main(String[] args)
+    {
+        int a = 'A'/'A';
+        String b = "..........";
+
+        for (int i=a;i<= (b.length()* b.length());i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
